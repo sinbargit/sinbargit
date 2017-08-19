6977b7ff1a7a075f04544e6bc6e53f2bd12f1053
sinbargit <sinbar.xing@outlook.com>
2017-08-19 12:32:44 +0800

    commit

--- src/xiaobai/server/start.java
+++ src/xiaobai/server/start.java
@@ -1,32 +1,23 @@
 package xiaobai.server;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
-import java.io.IOException;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.glassfish.jersey.servlet.ServletContainer;
+import org.glassfish.jersey.server.ServerProperties;
 
-public class start extends AbstractHandler
+public class Start
 {
-    public void handle(String target,
-                       Request baseRequest,
-                       HttpServletRequest request,
-                       HttpServletResponse response)
-            throws IOException, ServletException
-    {
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-        baseRequest.setHandled(true);
-        response.getWriter().println("<h1>Hello World</h1>");
-    }
-
     public static void main(String[] args) throws Exception
     {
         Server server = new Server(8080);
-        server.setHandler(new start());
-
+        ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        contextHandler.setContextPath("/");
+        server.setHandler(contextHandler);
+        ServletHolder holder = new ServletHolder (ServletContainer.class);
+        holder.setInitParameter(ServerProperties.PROVIDER_PACKAGES, "xiaobai.restful");
+        contextHandler.addServlet(holder,"/*");
+        holder.setInitOrder(1);
         server.start();
         server.join();
     }
