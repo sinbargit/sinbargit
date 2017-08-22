1ce6daca04046528054496ae3adec36ca6e4c5d1
sinbargit <sinbar.xing@outlook.com>
2017-08-22 18:24:32 +0800

    commit

--- src/xiaobai/restful/Resource.java
+++ src/xiaobai/restful/Resource.java
@@ -1,34 +1,75 @@
 package xiaobai.restful;
+
+import org.apache.jackrabbit.api.JackrabbitSession;
+import org.apache.jackrabbit.api.security.user.UserManager;
 import org.apache.jackrabbit.rmi.client.ClientRepositoryFactory;
 
-import javax.jcr.GuestCredentials;
-import javax.jcr.Repository;
-import javax.jcr.Session;
-import javax.ws.rs.Path;
-import javax.ws.rs.GET;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.jcr.*;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+
 @Path("resource")
 public class Resource {
+    private static Repository repository = null;
+    private static Session session = null;
+    private static UserManager userManager = null;
+
     @Path("hello")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() throws Exception {
         return "hello wolrd! ";
     }
-    @Path("rmi")
+
+    @Path("/{name}/{path}")
     @GET
-    public String rmi() throws Exception {
-        ClientRepositoryFactory factory = new ClientRepositoryFactory();
-        Repository repository = factory.getRepository("rmi://localhost:9000/content");
-        Session session = repository.login(new GuestCredentials());
-        try {
-            String user = session.getUserID();
-            String name = repository.getDescriptor(Repository.REP_NAME_DESC);
-            return name+"--"+user;
-        } finally {
-            session.logout();
+    public String get(@PathParam("name") String name, @HeaderParam("ps") String ps,@PathParam("path") String path) throws Exception {
+      this.checkUser(name,ps);
+      Session session = getRepository().login(new SimpleCredentials(name,ps.toCharArray()));
+      Node root = session.getRootNode();
+      return  root.getNode(path).getName();
+    }
+
+    @Path("/{name}/{path}")
+    @PUT
+    public void put(@PathParam("name") String name,@PathParam("path") String path, @HeaderParam("ps") String ps) throws Exception {
+        Session session = getRepository().login(new SimpleCredentials(name,ps.toCharArray()));
+        Node root = session.getRootNode();
+        root.addNode(path);
+    }
+
+    private Repository getRepository() throws Exception {
+        if (null == repository) {
+            ClientRepositoryFactory factory = new ClientRepositoryFactory();
+            repository = factory.getRepository("rmi://localhost:9000/content");
+        }
+        return repository;
+    }
+
+    private Session getSession() throws Exception
+    {
+        if(null == session)
+        {
+            session = getRepository().login(new SimpleCredentials("admin", "admin".toCharArray()));
+        }
+        return session;
+    }
+
+    private UserManager getUserManager() throws Exception
+    {
+        if(null == userManager)
+        {
+            userManager = ((JackrabbitSession)getSession()).getUserManager();
+        }
+        return userManager;
+    }
+
+    private void checkUser(String name,String password) throws Exception
+    {
+        UserManager userManager = getUserManager();
+        if(!userManager.findAuthorizables(name,name,UserManager.SEARCH_TYPE_USER).hasNext())
+        {
+            userManager.createUser(name,password);
         }
     }
 }
