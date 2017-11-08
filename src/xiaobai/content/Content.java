bfd6f02c7bae244c168c7c1f45acbb77213c2d04
sinbar.xie <sinbar.xing@outlook.com>
2017-11-08 21:26:17 +0800

    add cli

--- src/xiaobai/content/Content.java
+++ src/xiaobai/content/Content.java
@@ -5,6 +5,7 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.Naming;
 import java.util.Iterator;
 
+import org.apache.commons.cli.*;
 import org.apache.jackrabbit.commons.JcrUtils;
 import org.apache.jackrabbit.rmi.remote.RemoteRepository;
 import org.apache.jackrabbit.rmi.server.RemoteAdapterFactory;
@@ -14,7 +15,12 @@ import org.slf4j.Logger;
 import org.apache.log4j.BasicConfigurator;
 import xiaobai.content.service.UserService;
 
+import java.util.Scanner;
+
+
 public class Content {
+    private static UserService xiaobaiS = null;
+    private static Session xiaobai = null;
     public static void main(String[] args) throws Exception
     {
 
@@ -29,19 +35,6 @@ public class Content {
         }
 
         Repository repository = JcrUtils.getRepository();
-        Session session = repository.login(
-                new SimpleCredentials("admin", "admin".toCharArray()));
-        UserService adminS = new UserService(session);
-        Node root1 = session.getRootNode();
-        adminS.removeUser("xiaobai");
-        adminS.createUser("xiaobai","201314",root1);
-        Session xiaobai = repository.login(new SimpleCredentials("xiaobai", "201314".toCharArray()));
-        Node root = xiaobai.getRootNode();
-        UserService xiaobaiS = new UserService(xiaobai);
-        xiaobaiS.setRoot();
-        Node index = xiaobaiS.set("index");
-        xiaobaiS.set("index","src/xiaobai/content/index.md",index,"md");
-        xiaobaiS.sessionOut();
         LocateRegistry.createRegistry(9000);
         String name = "rmi://localhost:9000/content";
         RemoteAdapterFactory factory = new ServerAdapterFactory();
@@ -49,5 +42,72 @@ public class Content {
         Naming.bind(name,remote);
         Logger logger = LoggerFactory.getLogger(Content.class);
         logger.info("rmi on 9000");
+        Options options = new Options();
+        Option init = new Option("init",false,"init as user xiaobai");
+        Option add =  Option.builder("add").numberOfArgs(4).desc("desc of node").build();
+        Option delete = Option.builder("delete").hasArg().desc("path of node").build();
+        options.addOption(init);
+        options.addOption(add);
+        options.addOption(delete);
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("cli start");
+        while (scanner.hasNext())
+        {
+            String commend = scanner.nextLine();
+            String[] params = commend.split(" +");
+            CommandLineParser parser = new DefaultParser();
+            CommandLine cl = parser.parse(options,params);
+            if(cl.hasOption("init"))
+            {
+                Session session = repository.login(
+                        new SimpleCredentials("admin", "admin".toCharArray()));
+                UserService adminS = new UserService(session);
+                Node root = session.getRootNode();
+                adminS.removeUser("xiaobai");
+                adminS.createUser("xiaobai","201314",root);
+            }
+            else if(cl.hasOption("add"))
+            {
+                UserService xiaobaiS = getXiaobaiS(repository);
+                xiaobaiS.setRoot();
+                Node index = xiaobaiS.set("index");
+                String[] keys =  cl.getOptions()[0].getValues();
+                Node root = getXiaobai(repository).getRootNode();
+                Node node = null;
+                if(keys[2].equals("/"))
+                {
+                    node = root;
+                }
+                else
+                {
+                    node = root.getNode(keys[2]);
+                }
+                xiaobaiS.set(keys[0],keys[1],node,keys[3]);
+                xiaobaiS.sessionOut();
+            }
+            else if(cl.hasOption("delete"))
+            {
+                UserService xiaobaiS = getXiaobaiS(repository);
+                String path = cl.getOptions()[0].getValues()[0];
+                xiaobaiS.delete(path);
+            }
+        }
+    }
+    private static UserService getXiaobaiS(Repository repository) throws Exception
+    {
+        if(xiaobaiS==null)
+        {
+            Session xiaobai = getXiaobai(repository);
+            xiaobaiS = new UserService(xiaobai);
+        }
+        return xiaobaiS;
+    }
+    private static Session getXiaobai(Repository repository) throws Exception
+    {
+        if(xiaobai==null)
+        {
+            xiaobai = repository.login(new SimpleCredentials("xiaobai", "201314".toCharArray()));
+        }
+        return xiaobai;
     }
 }
